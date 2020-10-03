34cd72118107a04f2fab4253661c2658eb87d70c
alii13 <shekhaliul44@gmail.com>
2020-10-03 12:55:31 +0530

    [Feat ] Mobile Resposinve

--- src/Profiles.js
+++ src/Profiles.js
@@ -6,7 +6,7 @@ export class Profiles extends Component {
 
     state = {
         data: [],
-        placeHolder: [{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8}],
+        placeHolder: [{id:1},{id:2},{id:3},{id:4},{id:5},{id:6}],
         loading: true
     }
 
